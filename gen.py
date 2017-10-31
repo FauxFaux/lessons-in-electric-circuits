ea41f558c70c95b704ad537c72daf8d5e8853a55
Chris West (Faux) <git@goeswhere.com>
2017-10-31 19:57:35 +0000

    extract book order

--- gen.py
+++ gen.py
@@ -3,6 +3,106 @@ import os
 
 import ninja_syntax
 
+# for f in *(/); do echo "'$f': ["; egrep -h '^[A-Z]*_[0-9]*\.html' $f/Makefile \
+# | sed 's/.* : /'\''/; s/\.sml.*/'\'',/'; done
+
+BOOK = (
+    ('DC', [
+        'basiccon',
+        'ohm',
+        'safety',
+        'scientif',
+        's_and_p',
+        'divider',
+        's_p',
+        'dcmeter',
+        'dcsignal',
+        'dcnet',
+        'batt',
+        'conduct',
+        'cap',
+        'magnet',
+        'inductor',
+        'time',
+    ]),
+
+    ('AC', [
+        'basicac',
+        'complex',
+        'xzl',
+        'xzc',
+        'xzrlc',
+        'resonant',
+        'mixed',
+        'filters',
+        'trans',
+        'poly',
+        'acpower',
+        'acmeter',
+        'acmotor',
+        'lines',
+    ]),
+
+    ('Semi', [
+        'amplif',
+        'theory',
+        'diode',
+        'bjt',
+        'jfet',
+        'mosfet',
+        'thyr',
+        'opamp',
+        'analog',
+        'active',
+        'dcdrive',
+        'acdrive',
+        'tubes',
+    ]),
+
+    ('Digital', [
+        'numbers',
+        'binary',
+        'gates',
+        'switches',
+        'relays',
+        'ladder',
+        'boolean',
+        'karnaugh',
+        'combin',
+        'multivib',
+        'counters',
+        'shiftreg',
+        'a2d',
+        'commun',
+        'memory',
+        'computer',
+    ]),
+
+    ('Ref', [
+        'equation',
+        'colors',
+        'citable',
+        'algebra',
+        'trig',
+        'calculus',
+        'spice',
+        'trouble',
+        'symbol',
+        'periodic',
+    ]),
+
+    ('Exper', [
+        'intro',
+        'basic',
+        'dc',
+        'ac',
+        'discrete',
+        'a_ic',
+        'd_ic',
+        '555',
+    ]),
+)
+
 
 def main():
     n = ninja_syntax.Writer(open('.build.ninja~', 'w'))
