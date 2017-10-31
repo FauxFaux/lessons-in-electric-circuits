43c700c9cc7a8b2c35fd5b11a2c16367d56c4414
Chris West (Faux) <git@goeswhere.com>
2017-10-31 20:08:53 +0000

    detect duplicate images

--- gen.py
+++ gen.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 import os
+import os.path
+import re
+from typing import Iterable, Tuple, Iterator
 
 import ninja_syntax
 
@@ -103,12 +106,53 @@ BOOK = (
     ]),
 )
 
+IMAGE = re.compile('<image>([a-z0-9]+)\.(?:png|jpg|eps|gif)<')
+
+
+def volumes() -> Iterable[str]:
+    return [x[0] for x in BOOK]
+
+
+def available_images():
+    images = set()
+
+    for volume in volumes():
+        for file in os.listdir("{}/images".format(volume)):
+            root = os.path.splitext(file)[0]
+            name = (volume, root)
+
+            if name in images:
+                raise Exception("duplicate image: {}".format(name))
+
+            images.add(name)
+
+    return images
+
+
+def image_links(path: str) -> Iterator[Tuple[str, str]]:
+    with open(path) as f:
+        for line in f:
+            ma = IMAGE.search(line)
+            if ma:
+                yield ma.group(1)
+
+
+def required_images():
+    wanted = set()
+
+    for volume, chapters in BOOK:
+        for chapter in chapters:
+            wanted.update(image_links('{}/src/{}.sml'.format(volume, chapter)))
+
 
 def main():
     n = ninja_syntax.Writer(open('.build.ninja~', 'w'))
 
     n.rule('gen', ['./gen.py'])
     n.build('build.ninja', 'gen', './gen.py', variables={'generator': 1})
+
+    available_images()
+    required_images()
 
     n.close()
     os.rename('.build.ninja~', 'build.ninja')
